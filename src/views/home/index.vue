68ad2a591e2c74db10642e4e0d38fd348708f263
linkai <695658177@qq.com>
2020-03-02 22:30:54 +0800

    主页模块-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>我是主页</div>
+  <!-- 外层容器 -->
+  <el-container>
+    <!-- 左侧容器 -->
+    <el-aside>我是左侧容器</el-aside>
+    <!-- 右侧外层容器 -->
+    <el-container>
+      <!-- 顶部容器 -->
+      <el-header>我是顶部容器</el-header>
+      <!-- 主体容器 -->
+      <el-main>
+        <!-- 二级路由容器 -->
+        我是二级路由容器
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
