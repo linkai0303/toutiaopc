d309a30fae0790e7d0cb228f44724a9dfd344170
linkai <695658177@qq.com>
2020-03-08 15:42:00 +0800

    获取评论列表并完成分页功能

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -18,6 +18,10 @@
         </template>
       </el-table-column>
     </el-table>
+    <el-row style="height:80px" type='flex' align="middle" justify="center">
+        <!-- 分页组件 -->
+    <el-pagination background layout="prev, pager, next" :total="page.total" :current-page ="page.currentPage" :page-size="page.pageSize" @current-change="changePage"></el-pagination>
+    </el-row>
   </el-card>
 </template>
 
@@ -25,19 +29,32 @@
 export default {
   data () {
     return {
-      list: []
+      list: [],
+      page: {
+        total: 0, // 默认总页数
+        currentPage: 1, // 当前页码
+        pageSize: 10 // 每页条数
+      }
     }
   },
   methods: {
+    // 页码改变事件 newPage为最新的切换页码
+    changePage (newPage) {
+      this.page.currentPage = newPage // 最新页码赋值给当前页码
+      this.getComment()
+    },
     //   获取评论数据
     getComment () {
       this.$axios({
         url: '/articles',
         params: {
-          response_type: 'comment' // 控制获取数据类型
+          response_type: 'comment', // 控制获取数据类型
+          page: this.page.currentPage, // 查第一页数据
+          per_page: this.page.pageSize// 每页查十条
         }
       }).then(result => {
-        this.list = result.data.results
+        this.list = result.data.results// 将数据赋值给list
+        this.page.total = result.data.total_count // 将总数赋值
       })
     },
     // 格式化布尔值
