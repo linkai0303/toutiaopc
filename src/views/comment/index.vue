c06e4fd5efb20c1d8a8ed7996d7da67b2eeeaf3e
linkai <695658177@qq.com>
2020-03-08 09:41:34 +0800

    评论列表页面结构搭建

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,13 +1,31 @@
 <template>
-<div>123</div>
+  <el-card>
+    <!-- 卡片头部放置一个面包屑组件 -->
+    <bread-crumb slot="header">
+      <template slot="title">评论管理</template>
+    </bread-crumb>
+    <!-- 表格组件 -->
+    <el-table :data="list">
+      <el-table-column width="800"  label="标题"></el-table-column>
+      <el-table-column  label="评论状态"></el-table-column>
+      <el-table-column  label="总评论数"></el-table-column>
+      <el-table-column  label="粉丝论数"></el-table-column>
+      <el-table-column  label="操作"></el-table-column>
+    </el-table>
+  </el-card>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      list: [
 
+      ]
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
