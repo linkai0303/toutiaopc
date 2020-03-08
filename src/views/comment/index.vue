2394dc4ab25652bc9aae8521c3d61793b7896038
linkai <695658177@qq.com>
2020-03-08 10:42:43 +0800

    加载评论数据及状态过滤

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -6,11 +6,15 @@
     </bread-crumb>
     <!-- 表格组件 -->
     <el-table :data="list">
-      <el-table-column width="800"  label="标题"></el-table-column>
-      <el-table-column  label="评论状态"></el-table-column>
-      <el-table-column  label="总评论数"></el-table-column>
-      <el-table-column  label="粉丝论数"></el-table-column>
-      <el-table-column  label="操作"></el-table-column>
+      <el-table-column width="800" prop="title" label="标题"></el-table-column>
+      <!-- table中不显示布尔值 用formatter属性用来格式化内容 -->
+      <el-table-column :formatter="formatterBool" prop="comment_status" label="评论状态"></el-table-column>
+      <el-table-column prop="total_comment_count" label="总评论数"></el-table-column>
+      <el-table-column prop="fans_comment_count" label="粉丝论数"></el-table-column>
+      <el-table-column label="操作">
+          <el-button type="text">修改</el-button>
+          <el-button type="text">关闭评论</el-button>
+      </el-table-column>
     </el-table>
   </el-card>
 </template>
@@ -19,10 +23,27 @@
 export default {
   data () {
     return {
-      list: [
-
-      ]
+      list: []
     }
+  },
+  methods: {
+    getComment () {
+      this.$axios({
+        url: '/articles',
+        params: {
+          response_type: 'comment'// 控制获取数据类型
+        }
+      }).then(result => {
+        this.list = result.data.results
+      })
+    },
+    // 格式化布尔值
+    formatterBool (row, column, cellValue, index) {
+      return cellValue ? '正常' : '关闭'
+    }
+  },
+  created () {
+    this.getComment()
   }
 }
 </script>
