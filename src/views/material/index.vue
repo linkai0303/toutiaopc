dd4cbae744209f593b7ddf27e49ed48caee856a4
linkai <695658177@qq.com>
2020-03-09 17:50:54 +0800

    素材列表数据加载

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,20 +1,30 @@
 <template>
   <el-card>
-      <!-- 放置面包屑组件 -->
-      <bread-crumb slot="header">
-        <template slot="title">
-            素材管理
-        </template>
-      </bread-crumb>
-      <!-- 放置标签页 v-model所绑定的值就是当前激活的页签 -->
-      <el-tabs v-model="activeName">
-          <el-tab-pane label="全部素材" name="all">
-              全部
-          </el-tab-pane>
-          <el-tab-pane label="收藏素材" name="collect">
-              收藏
-          </el-tab-pane>
-      </el-tabs>
+    <!-- 放置面包屑组件 -->
+    <bread-crumb slot="header">
+      <template slot="title">素材管理</template>
+    </bread-crumb>
+    <!-- 放置标签页 v-model所绑定的值就是当前激活的页签 -->
+    <el-tabs v-model="activeName" @tab-click="changeTab">
+      <el-tab-pane label="全部素材" name="all">
+        <div class="img-list">
+          <el-card v-for="item in list" :key="item.id" class="img-card">
+            <img :src="item.url" alt />
+            <el-row class="operate" type="flex" align="middle" justify="space-around">
+              <i class="el-icon-star-on"></i>
+              <i class="el-icon-delete-solid"></i>
+            </el-row>
+          </el-card>
+        </div>
+      </el-tab-pane>
+      <el-tab-pane label="收藏素材" name="collect">
+        <div class="img-list">
+          <el-card v-for="item in list" :key="item.id" class="img-card">
+            <img :src="item.url" alt />
+          </el-card>
+        </div>
+      </el-tab-pane>
+    </el-tabs>
   </el-card>
 </template>
 
@@ -22,12 +32,57 @@
 export default {
   data () {
     return {
-      activeName: 'all'// 当前激活的页签
+      activeName: 'all', // 当前激活的页签
+      list: [] // 用来接收全部素材
     }
+  },
+  methods: {
+    getMeterial () {
+      this.$axios({
+        url: '/user/images',
+        params: {
+          collect: this.activeName === 'collect' // 根据当前activeName 决定获取的数据是否为收藏数据
+        },
+        data: {}
+      }).then(result => {
+        this.list = result.data.results // 将返回的数据赋值到data中的数据
+      })
+    },
+    changeTab () {
+      this.getMeterial()
+    }
+  },
+  created () {
+    this.getMeterial()
   }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.img-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  .img-card {
+    position: relative;
+    width: 220px;
+    height: 240px;
+    margin: 20px 50px;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+    .operate {
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      width: 100%;
+      height: 30px;
+      background-color: #f4f5f6;
+      i {
+        font-size: 20px;
+      }
+    }
+  }
+}
 </style>
