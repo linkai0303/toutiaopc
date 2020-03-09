43dc14f8868a2ce58d17d6fed7e526450ae3627e
linkai <695658177@qq.com>
2020-03-09 19:43:01 +0800

    素材列表分页功能

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -25,6 +25,15 @@
         </div>
       </el-tab-pane>
     </el-tabs>
+    <el-row type="flex" justify="center" align="middle" style="height:80px">
+        <el-pagination background
+         :total="page.total"
+        :current-page="page.currentpage"
+        :page-size="page.pageSize"
+        layout="prev,pager,next"
+        @current-change="changePage"
+        ></el-pagination>
+    </el-row>
   </el-card>
 </template>
 
@@ -33,22 +42,36 @@ export default {
   data () {
     return {
       activeName: 'all', // 当前激活的页签
-      list: [] // 用来接收全部素材
+      list: [], // 用来接收全部素材
+      page: {
+        currentPage: 1, // 默认加载第一页
+        total: 0, // 当前总数
+        pageSize: 8// 每页多少条
+      }
     }
   },
   methods: {
+    changePage (newPage) {
+      this.page.currentPage = newPage
+      this.getMeterial()
+    },
     getMeterial () {
       this.$axios({
         url: '/user/images',
         params: {
-          collect: this.activeName === 'collect' // 根据当前activeName 决定获取的数据是否为收藏数据
+          collect: this.activeName === 'collect', // 根据当前activeName 决定获取的数据是否为收藏数据
+          page: this.page.currentPage, // 获取当前页码中的数据
+          per_page: this.page.pageSize // 获取每页数量
         },
         data: {}
       }).then(result => {
         this.list = result.data.results // 将返回的数据赋值到data中的数据
+        this.page.total = result.data.total_count // 将素材总数赋值给当前总数
       })
     },
     changeTab () {
+      // 将页码重置为第一页
+      this.page.currentPage = 1
       this.getMeterial()
     }
   },
