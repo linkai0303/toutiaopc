f04a2bce3294a167510e660b3b7d512bb85692b1
linkai <695658177@qq.com>
2020-03-11 23:37:16 +0800

    分页功能完成

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -7,7 +7,7 @@
     <el-form style="padding-left:50px">
       <el-form-item label="文章状态 : ">
         <!-- 单选框组 -->
-        <el-radio-group v-model="searchForm.status" @change="changeCondition">
+        <el-radio-group v-model="searchForm.status">
           <el-radio :label="5">全部</el-radio>
           <el-radio :label="0">草稿</el-radio>
           <el-radio :label="1">待审核</el-radio>
@@ -17,14 +17,14 @@
       </el-form-item>
       <el-form-item label="频道类型 : ">
         <!-- 选择器 -->
-        <el-select @change="changeCondition" placeholder="请选择频道" v-model="searchForm.channel_id">
+        <el-select placeholder="请选择频道" v-model="searchForm.channel_id">
           <!-- 下拉选项组件 label=>显示值 value=>绑定值 -->
           <el-option v-for="item in channels" :key="item.id" :label="item.name" :value="item.id"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="日期范围 : ">
         <!-- 日期范围选择组件 -->
-        <el-date-picker @change="changeCondition" type="daterange" value-format="yyyy-MM-dd" v-model="searchForm.dateRange"></el-date-picker>
+        <el-date-picker type="daterange" value-format="yyyy-MM-dd" v-model="searchForm.dateRange"></el-date-picker>
       </el-form-item>
     </el-form>
     <!-- -------------------------文章主体结构 ---------------------------------->
@@ -43,14 +43,20 @@
         </div>
       </div>
       <div class="right">
-        <span>
-          <i class="el-icon-edit"></i> 修改
-        </span>
-        <span>
-          <i class="el-icon-delete"></i> 删除
-        </span>
+        <span><i class="el-icon-edit"></i> 修改</span>
+        <span><i class="el-icon-delete"></i> 删除</span>
       </div>
     </div>
+  <!-- 放置分页组件 -->
+  <el-row type="flex" justify="center" style="height:80px" align="middle">
+    <el-pagination :current-page="page.currentPage"
+    :page-size="page.pageSize"
+    :total="page.total"
+    @current-change="changePage"
+    background layout='prev,pager,next'>
+
+    </el-pagination>
+  </el-row>
   </el-card>
 </template>
 
@@ -58,6 +64,11 @@
 export default {
   data () {
     return {
+      page: {
+        currentPage: 1,
+        pageSize: 10,
+        total: 0
+      },
       // 表单数据对象
       searchForm: {
         // 文章状态:0-草稿;1-待审核;2-审核通过;3-审核失败;4-已删除;5-不传为全部
@@ -68,6 +79,17 @@ export default {
       dateRange: [], // 接收频道的数据
       list: [], // 接收文章列表
       defaultImg: require('@/assets/yourname.jpeg') // 图片地址对应文件赋值给变量
+    }
+  },
+  // 监听data中的数据变化
+  watch: {
+    searchForm: {
+      deep: true, // 深度检测searchForm中的数据变化
+      // 数据发生变化就会触发
+      handler () {
+        this.page.currentPage = 1 // 条件变化 重置页码
+        this.changeCondition() // 调用值改变方法发
+      }
     }
   },
   //   专门处理 显示格式
@@ -100,18 +122,25 @@ export default {
     }
   },
   methods: {
+    // 改变页码
+    changePage (newPage) {
+      this.page.currentPage = newPage // 赋值最新页码
+      this.changeCondition() // 调用改变事件方法
+    },
     // 条件改变事件方法
     changeCondition () {
       // 组装筛选的条件params
       const params = {
+        page: this.page.currentPage,
+        per_page: this.page.pageSize,
         // 文章状态
         status: this.searchForm.status === 5 ? null : this.searchForm.status,
         // 表单数据
         channel_id: this.searchForm.channel_id,
         // 开始时间
-        begin_pubdate: this.searchForm.dateRange ? this.searchForm.dateRange[0] : null,
+        begin_pubdate: this.searchForm.dateRange && this.searchForm.dateRange.length ? this.searchForm.dateRange[0] : null,
         // 结束时间
-        end_pubdate: this.searchForm.dateRange ? this.searchForm.dateRange[1] : null
+        end_pubdate: this.searchForm.dateRange && this.searchForm.dateRange.length ? this.searchForm.dateRange[1] : null
       }
       this.getArticles(params) // 调用获取文章列表方法并传入参数
     },
@@ -121,7 +150,8 @@ export default {
         url: '/articles',
         params
       }).then(result => {
-        this.list = result.data.results
+        this.list = result.data.results // 获取文章列表
+        this.page.total = result.data.total_count // 赋值总数
       })
     },
     //   获取频道数据
