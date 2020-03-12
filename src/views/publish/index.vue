b6ab06cb01cb57ea836023a74e9a546002eedd78
linkai <695658177@qq.com>
2020-03-12 16:17:39 +0800

    发表文章页面简单布局

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -1,8 +1,34 @@
 <template>
   <el-card>
+      <!-- 面包屑组件 -->
       <bread-crumb slot="header">
         <template slot="title">发布文章</template>
       </bread-crumb>
+    <!-- 表单组件 -->
+    <el-form style="margin-left:50px">
+        <el-form-item label="标题">
+            <el-input placeholder="请输入您的标题" style="width:60%"></el-input>
+        </el-form-item>
+        <el-form-item label="内容">
+            <el-input placeholder="请输入您的内容" type="textarea" :rows="4"></el-input>
+        </el-form-item>
+        <el-form-item label="封面">
+            <!-- 单选框组 -->
+            <el-radio-group>
+                <el-radio>单图</el-radio>
+                <el-radio>三图</el-radio>
+                <el-radio>无图</el-radio>
+                <el-radio>自动</el-radio>
+            </el-radio-group>
+        </el-form-item>
+        <el-form-item label="频道">
+            <el-select placeholder="请选择频道"></el-select>
+        </el-form-item>
+        <el-form-item>
+            <el-button type="primary">发表</el-button>
+            <el-button>存入草稿</el-button>
+        </el-form-item>
+    </el-form>
   </el-card>
 </template>
 
